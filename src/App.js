67215e55ec353a076983acc0cae4d3d6d69733f9
doomdev69 <singhavicool@gmail.com>
2020-06-28 00:34:36 +0530

    style collection-item, add and style header

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
+import Header  from './components/header/header.component';
 import './App.css';
 
 function App() {
   return (
     <div>
-       <Route exact path='/' component={HomePage} />
-       <Route path='/shop' component={ShopPage} />
+      <Header />
+      <div>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
